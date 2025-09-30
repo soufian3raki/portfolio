32e0a0cc223803efbd0cf0d64201b052282e29db
Soufiane <cv@soufiane.es>
2025-10-01 00:16:14 +0200

    🔧 Actualiza la configuración del portafolio personal de Soufiane Raki, cambiando la URL del sitio y añadiendo soporte para despliegue en GitHub Pages. Se actualizan las dependencias y se realizan ajustes en los enlaces sociales y estilos de las páginas para mejorar la presentación y la usabilidad.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://soufiane-raki.com',
+    site: 'https://soufian3raki.github.io/portfolio/',
+    //base: '/portfolio/',
     markdown: {
         shikiConfig: {
             theme: 'dracula',
